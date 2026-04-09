6a3a69340e3831fc113ed9f66961e6f35daa8ed9
Runa <runa.uxlabs@gmail.com>
2026-04-09 15:45:19 +0900

    github action 첫배포

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import { Calendar } from "@/components/ui/calendar";
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
 
   return (
     <>
-      안녕하쇼
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-lg border"
+      />
     </>
   );
 }
